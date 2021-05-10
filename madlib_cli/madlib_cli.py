53d6ab2183eacb565a61e2f7a5260253f7e61af5
Omar-zoubi <o.fandi02@gmail.com>
2021-05-10 14:43:29 +0300

    lab03

--- madlib_cli/madlib_cli.py
+++ madlib_cli/madlib_cli.py
@@ -6,10 +6,10 @@ def reading_file(path):
 
     try:
       with open(path, 'r') as file:
-       new_file= file.read().strip()
+       new_file= file.read()
        return new_file
     except FileNotFoundError:
-      raise FileNotFoundError('404 file not found.')
+      raise FileNotFoundError('404 file not found')
     except:
        print("Some thing happend")
 
@@ -18,6 +18,8 @@ def reading_file(path):
 def filter_template(value):
     rgx = r"{(.*?)}"
     needed_value= re.findall(rgx, value)
+    return needed_value
+def inp_user():
     print("Plz answer these Q: ")
     qCounter=1
     userAnsewrs = []
@@ -26,8 +28,8 @@ def filter_template(value):
         userAnsewrs.append(input(f"Q{qCounter} Plz Give a {i}: "))
         qCounter+=1
 
-    result = [userAnsewrs, needed_value]
-    return result
+    return userAnsewrs
+   
 
 def render_kk():
     editting  = reading_file('madlib_cli/template.txt')
@@ -37,7 +39,8 @@ def save_to_anew(result):
     with open("madlib_cli/newfile.txt", "w") as file:
         file.write(result)
 
-def merge(user_answer, needed_value , wfile):
+def merge(wfile, user_answer):
+    needed_value=filter_template(wfile)
     i = 0
     for x in user_answer:
         wfile = wfile.replace(needed_value[i], x , 1)
@@ -47,11 +50,13 @@ def merge(user_answer, needed_value , wfile):
  
     print(show_value)
 
-    return show_value  
+    return show_value 
+
+
+
 if __name__ == "__main__":
     file_to_open = reading_file('madlib_cli/template.txt')
     getting_values = filter_template(file_to_open)
     user_answer = getting_values[0]
     needed_value = getting_values[1] 
-    merge(user_answer, needed_value, file_to_open)
-    save_to_anew(merge(user_answer, needed_value, file_to_open))
+    merge(file_to_open, user_answer)
